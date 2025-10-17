def1aa0fc9f092b461817e19423b47f90171a8b2
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-10-17 10:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.88%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -4,11 +4,8 @@ public:
         if(k%2==0||k%5==0){
             return -1;
         }
-        if(k==1){
-            return 1;
-        }
-        int n=11;
-        int cnt=2;
+        int n=1;
+        int cnt=1;
         while(1){
             int a=n%k;
             if(a==0){
