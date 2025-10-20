277b9798dc163e9b6c4ac9cfebae7c92bcd915cf
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-10-20 13:45:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (10.37%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int cnt=0,o=0;
+        int cnt=0;
         for(int i=0;i<operations.size();i++){
             string a=operations[i];
             if(a=="X++"||a=="++X"){
                 cnt++;
             }
             else{
-                o++;
+                cnt--;
             }
         }
-        return cnt-o;
+        return cnt;
     }
 };
