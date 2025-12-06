1514be7fd1803939ec32aeb7f05059099d63cdbd
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-06 17:32:29 +0530

    Time: 39 ms (24.13%) | Memory: 106.7 MB (79.35%) - LeetSync

---
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(),asteroids.end());
+        long long a=mass;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>a){
+                return false;
+            }
+            a+=asteroids[i];
+        }
+        return true;
+    }
+};
