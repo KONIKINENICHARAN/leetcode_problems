888160cc55de66c6e4828c7ab6baaec19042920b
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-09 10:55:28 +0530

    Time: 725 ms (64.23%) | Memory: 422.6 MB (52.92%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -8,12 +8,19 @@ public:
         }
          unordered_map<int,long long>fre;
          for(int i=0;i<nums.size()-1;i++){
-            freq[nums[i]]--;
-           if(fre[nums[i]*2]!=0 and freq[nums[i]*2]!=0){
-            cnt+=((fre[nums[i]*2]*freq[nums[i]*2])%1000000007);
-            cnt=cnt%1000000007;
-           }
-           fre[nums[i]]++;
+            fre[nums[i]]++;
+            if(fre[2*nums[i]]!=0){
+                int k=abs(freq[2*nums[i]]-fre[2*nums[i]]);
+                if(2*nums[i]==0){
+                    if(fre[nums[i]]>1){
+                        int k=abs(freq[2*nums[i]]-(fre[2*nums[i]]));
+                        cnt=(cnt+(fre[2*nums[i]]-1)*k)%1000000007;
+                    }
+                }
+                else{
+                cnt=(cnt+fre[2*nums[i]]*k)%1000000007;
+                }
+            }
          }
         return cnt;
     }
