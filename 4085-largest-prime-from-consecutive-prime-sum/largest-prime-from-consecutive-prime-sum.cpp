4398552f1a492878b597edbaf6358a06bba6b7e8
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-09 11:46:56 +0530

    Time: 704 ms (55.18%) | Memory: 335.6 MB (7.68%) - LeetSync

---
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int largestPrime(int n) {
+        if(n<=1){
+            return 0;
+        }
+        int k=2;
+        vector<int>A(n+1,1);
+        A[0]=0;
+        A[1]=1;
+        for(int i=2;i<=sqrt(n);i++){
+            if(A[i]){
+                for(int j=i+i;j<=n;j+=i){
+                    A[j]=0;
+                }
+            }
+        }
+        vector<int>C;
+        for(int i=2;i<=n;i++){
+            if(A[i]){
+                C.push_back(i);
+            }
+        }
+        int maxi=2,s=2;
+        for(int i=1;i<C.size();i++){
+            maxi=maxi+C[i];
+            if(maxi>n){
+                break;
+            }
+            if(binary_search(C.begin(),C.end(),maxi)){
+                s=maxi;
+            }
+        }
+        return s;
+    }
+};
