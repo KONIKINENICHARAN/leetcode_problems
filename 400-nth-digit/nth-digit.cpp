b983c16cfe6c8cc326dc0d376a86ff1eaf731803
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-09 19:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (5.21%) - LeetSync

---
+++ 400-nth-digit/nth-digit.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findNthDigit(int n) {
+        if(n<=9){
+            return n;
+        }
+        vector<long long>A;
+        long long x=1;
+        for(int i=0;i<10;i++){
+            long long a=9*pow(10,x-1);
+             a=a*x;
+              A.push_back(a);
+            x++;
+        }
+         long long j=n,sum=0;
+         int k=0;
+        for(int i=0;i<9;i++){
+            if(j<=A[i]){
+                k=i+1;
+                break;
+            }
+            j=j-A[i];
+            sum+=A[i];
+        }
+        long long h=n-sum;
+        long long b=k-1;
+        string w="";
+        while(b>0){
+            w+='9';
+            b--;
+        }
+        long long op=0;
+        if(!w.empty()){
+            op+=stoll(w);
+        }
+        op+=(h+k-1)/k;
+        w=to_string(op);
+        cout<<w;
+        int oi=(h-1)%k;
+        return w[oi]-'0';
+    }
+};
