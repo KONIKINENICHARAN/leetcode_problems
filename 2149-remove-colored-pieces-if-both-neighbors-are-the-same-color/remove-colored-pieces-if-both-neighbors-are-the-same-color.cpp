e77dcbcc024c1ea4fcb282fa8b79abb53a21069b
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-19 15:06:29 +0530

    Time: 7 ms (43.81%) | Memory: 15.6 MB (72.94%) - LeetSync

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int cnt=0,o=0,i=0;
+        if(colors.size()<=2){
+            return 0;
+        }
+        while(i<colors.size()-2){
+            if(colors[i]=='A'&&colors[i+1]=='A'&&colors[i+2]=='A'){
+                cnt++;
+            }
+            i++;
+        }
+        i=0;
+        while(i<colors.size()-2){
+            if(colors[i]=='B'&&colors[i+1]=='B'&&colors[i+2]=='B'){
+                o++;
+            }
+            i++;
+        }
+        if(cnt>o){
+            return 1;
+        }
+         return 0;   
+    }
+};
