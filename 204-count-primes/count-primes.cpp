6d63254001d45fb54dace8fff7c63a89f01f6159
KONIKINENICHARAN <charanchowdary644@gmail.com>
2025-12-19 10:11:04 +0530

    Time: 174 ms (61.35%) | Memory: 132.6 MB (13.37%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -6,8 +6,6 @@ public:
         }
         vector<int>A(n,1);
         int cnt=0;
-        A[0]=0;
-        A[1]=0;
         for(int i=2;i<=sqrt(n);i++){
             if(A[i]!=0){
                 for(int j=i+i;j<n;j+=i){
@@ -15,7 +13,7 @@ public:
                 }
             }
         }
-        for(int i=0;i<n;i++){
+        for(int i=2;i<n;i++){
             if(A[i]==1){
                 cnt++;
             }
