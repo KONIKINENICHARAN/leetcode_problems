0d39c1471c1e7ba133167fca5fef96b834a2e314
KONIKINENICHARAN <charanchowdary644@gmail.com>
2026-01-08 13:55:54 +0530

    Time: 19 ms (29.73%) | Memory: 34.2 MB (38.59%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int cnt=0,o=0;
+        stack<char>A;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='['){
+                cnt++;
+            }
+            else{
+                if(cnt>0){
+                    cnt--;
+                }
+                else{
+                    o++;
+                }
+            }
+        }
+        return (o+1)/2;
+    }
+};
