3a6f66ed06511ec2017ea119f14ee3e7224b6394
KONIKINENICHARAN <charanchowdary644@gmail.com>
2026-01-27 19:50:23 +0530

    Time: 20 ms (66.24%) | Memory: 102.9 MB (70.68%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -4,7 +4,6 @@ public:
         vector<int>A(temperatures.size(),0);
         stack<int>st;
         for(int i=temperatures.size()-1;i>=0;i--){
-            int j=i;
             while(!st.empty()&&temperatures[i]>=temperatures[st.top()]){
                 st.pop();
             }
