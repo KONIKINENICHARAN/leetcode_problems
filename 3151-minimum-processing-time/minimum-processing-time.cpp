21875658f919151a6c30af3152c2a2fe2f17d633
KONIKINENICHARAN <charanchowdary644@gmail.com>
2026-01-29 13:47:54 +0530

    Time: 56 ms (28.91%) | Memory: 104.9 MB (13.57%) - LeetSync

---
+++ 3151-minimum-processing-time/minimum-processing-time.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minProcessingTime(vector<int>& processorTime, vector<int>& tasks) {
+        sort(processorTime.begin(),processorTime.end());
+        sort(tasks.rbegin(),tasks.rend());
+         int maxi=processorTime[0]+tasks[0];
+        int i=1,j=0;
+        while(i<processorTime.size()){
+            if(j+4<tasks.size()){
+               maxi=max(maxi,processorTime[i]+tasks[j+4]);
+               j+=4;
+            }
+            i++;
+        }
+        return maxi;
+    }
+};
