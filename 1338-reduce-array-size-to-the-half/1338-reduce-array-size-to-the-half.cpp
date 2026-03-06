e1979b3c596472e80742aaf146e1ea9ae3359926
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-03-06 21:12:12 +0530

    Time: 143 ms (13.33%), Space: 86.8 MB (6.61%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int k=arr.size();
+        int op=k/2;
+        map<int,int>freq;
+        for(int i=0;i<k;i++){
+            freq[arr[i]]++;
+        }
+        vector<pair<int,int>>A;
+        for(auto it:freq){
+            A.push_back({it.second,it.first});
+        }
+        sort(A.begin(),A.end());
+        int cnt=0;
+        for(int i=A.size()-1;i>=0;i--){
+            k=k-A[i].first;
+            cnt++;
+            if(k<=op){
+                break;
+            }
+             
+        }
+      return cnt;
+    }
+};
