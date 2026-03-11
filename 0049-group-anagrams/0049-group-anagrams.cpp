e7a41bc402a3b1813c837a4a6ed6e230f30d6feb
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-03-11 12:00:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string,vector<int>>freq;
+        unordered_map<string,vector<int>>freq;
         for(int i=0;i<strs.size();i++){
             string op=strs[i];
             sort(op.begin(),op.end());
