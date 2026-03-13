ff1d41eb612220c111276c07a1e58a0ffc2fe638
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-03-13 21:34:36 +0530

    Time: 2555 ms (5.16%), Space: 27.2 MB (10.71%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        map<int,int>freq;
+        for(int i=0;i<arr.size();i++){
+            freq[arr[i]]++;
+        }
+        long long cnt=0;
+        for(int j=0;j<arr.size()-2;j++){
+            freq[arr[j]]--;
+            if(freq[arr[j]]==0){
+                freq.erase(arr[j]);
+            }
+            for(int k=j+1;k<arr.size();k++){
+                freq[arr[k]]--;
+                if(freq[arr[k]]==0){
+                    freq.erase(arr[k]);
+                }
+                int op=target-arr[j]-arr[k];
+                if(freq.count(op)){
+                    cnt+=freq[op];
+                }
+                cnt=cnt%1000000007;
+                freq[arr[k]]++;
+            }
+        }
+        return cnt/2;
+    }
+};
