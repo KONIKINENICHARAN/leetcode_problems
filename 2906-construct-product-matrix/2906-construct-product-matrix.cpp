5e60a342ee5c61d79cf809ae1d6a87d658007fbe
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-03-24 14:08:25 +0530

    Time: 106 ms (16.24%), Space: 172.9 MB (5.9%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -10,8 +10,8 @@ public:
                 cnt++;
             }
         }
-        vector<long long>prefix(cnt,1);
-        vector<long long>suffix(cnt,1);
+        vector<long long>prefix(cnt);
+        vector<long long>suffix(cnt);
         prefix[0]=1;
         suffix[cnt-1]=1;
         for(int i=1;i<a.size();i++){
@@ -21,41 +21,33 @@ public:
             suffix[i]=(suffix[i+1]*a[i+1])%12345;
         }
         vector<vector<int>>ans;
-        int k=0,p=0;
-        int cntt=0;
-          for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]==0){
-                    if(cntt>1){
-                        for(int i=0;i<grid.size();i++){
-                            vector<int>A;
-                            for(int j=0;j<grid[i].size();j++){
-                                A.push_back(0);
-                            }
-                            ans.push_back(A);
-                        }
-                        return ans;
-                    }
-                    cntt++;
-                    k=i;
-                    p=j;
-                }
-            }
-        }
+        // int k=0,p=0;
+        // int cntt=0;
+        //   for(int i=0;i<grid.size();i++){
+        //     for(int j=0;j<grid[i].size();j++){
+        //         if(grid[i][j]==0){
+        //             if(cntt>1){
+        //                 for(int i=0;i<grid.size();i++){
+        //                     vector<int>A;
+        //                     for(int j=0;j<grid[i].size();j++){
+        //                         A.push_back(0);
+        //                     }
+        //                     ans.push_back(A);
+        //                 }
+        //                 return ans;
+        //             }
+        //             cntt++;
+        //             k=i;
+        //             p=j;
+        //         }
+        //     }
+        // }
         int op=0;
           for(int i=0;i<grid.size();i++){
             vector<int>A;
             long long pro=1;
             for(int j=0;j<grid[i].size();j++){
-                //     if(op==0){
-                //         pro=suffix[op];
-                //     }
-                //    else if(op==cnt-1){
-                //         pro=prefix[op];
-                //     }
-                    
-                        pro=(prefix[op]*suffix[op])%12345;
-                    
+                 pro=(prefix[op]*suffix[op])%12345;
                     op++;
                    A.push_back(pro);
             }
