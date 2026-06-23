b65230773d3812b96881bcba58efdfba9b4c4fc4
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-06-23 13:43:00 +0530

    Time: 21 ms (16.57%), Space: 261.9 MB (92.31%) - LeetHub

---
+++ 3096-minimum-levels-to-gain-more-points/3096-minimum-levels-to-gain-more-points.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumLevels(vector<int>& possible) {
+        int sum=0;
+        if(possible.size()==2){
+            if(possible[0]==1&&possible[1]==0){
+                return 1;
+            }
+            else{
+                return -1;
+            }
+        }
+        for(int i=0;i<possible.size();i++){
+            if(possible[i]==0){
+                sum--;
+            }
+            else{
+                sum++;
+            }
+        }
+        int s=0;
+        for(int i=0;i<possible.size()-1;i++){
+            if(possible[i]==1){
+                s++;
+            }
+            else{
+                s--;
+            }
+            int op=sum-s;
+            if(s>op){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+};
