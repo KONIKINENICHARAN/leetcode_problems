0787ef093ab197280757ca7735116ee09c94c547
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-06-30 12:09:57 +0530

    Time: 18 ms (27.78%), Space: 11 MB (81.8%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+     int i=0;
+     int j=i;
+     int freq[3]={0};
+     int cnt=0;
+       while(i<s.size()){
+        
+          
+           while(j<s.size()&&(freq[0]==0||freq[1]==0||freq[2]==0)){
+                freq[s[j]-'a']++;
+                j++;
+           }
+          // cout<<j<<" ";
+           if(i<j&&freq[0]!=0&&freq[1]!=0&&freq[2]!=0){
+                freq[s[i]-'a']--;
+                cnt+=(s.size()-j+1);
+              
+           }
+             i++;
+        //    i=j;
+       }
+       return cnt;
+    }
+};
