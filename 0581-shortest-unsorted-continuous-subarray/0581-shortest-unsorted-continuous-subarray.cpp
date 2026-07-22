249069b9fdf9d4c73648ba8ce6fdb38a0f1406fb
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-07-22 12:29:55 +0530

    Time: 7 ms (18.29%), Space: 34.8 MB (5.52%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,68 +1,23 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        stack<int>A;
-        int op=INT_MAX;
-         int io=INT_MAX;
-         int fla=0;
-         int ry=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            if(fla==0){
-            if(A.empty()||A.top()<=nums[i]){
-                ry=max(ry,nums[i]);
-                A.push(nums[i]);
-            }
-            else{
-                op=nums[i];
-                 io=min(io,nums[i]);
-                ry=max(ry,nums[i]);
+           vector<pair<int,int>>A;
+           for(int i=0;i<nums.size();i++){
+               A.push_back({nums[i],i});
+           } 
+           sort(A.begin(),A.end());
+           int mini=INT_MAX;
+           int maxi=INT_MIN,fla=0;
+           for(int i=0;i<A.size();i++){
+            if(i!=A[i].second){
+                maxi=max(maxi,A[i].second);
+                mini=min(mini,A[i].second);
                 fla=1;
             }
-            }
-            else{
-             io=min(io,nums[i]);
-                ry=max(ry,nums[i]);
-            }
-        }
-        if(fla==0){
+           }
+           if(fla==0){
             return 0;
-        }
-        fla=0;
-        stack<int>B;
-        int kl=-1;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(fla==0){
-            if(B.empty()||B.top()>=nums[i]){
-                B.push(nums[i]);
-            }
-            else{
-                kl=nums[i];
-                io=min(io,nums[i]);
-                ry=max(ry,nums[i]);
-                fla=1;
-            }
-            }
-            else{
-                io=min(io,nums[i]);
-                ry=max(ry,nums[i]);
-            }
-        }
-        while(!A.empty()&&A.top()>op){
-            A.pop();
-        }
-        while(!B.empty()&&B.top()<kl){
-            B.pop();
-        }
-        int u=nums.size()-(A.size()+B.size());
-        int o=1;
-        while(!A.empty()&&A.top()>io){
-            u++;
-            A.pop();
-        }
-        while(!B.empty()&&B.top()<ry){
-            u++;
-            B.pop();
-        }
-        return u;
+           }
+           return (maxi-mini+1);
     }
 };
