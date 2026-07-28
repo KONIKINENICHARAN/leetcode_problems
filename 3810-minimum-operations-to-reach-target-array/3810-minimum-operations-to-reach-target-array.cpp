954f54e3f29194d92122cd44dbe5543336d692b6
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-07-28 15:46:54 +0530

    Time: 159 ms (16.21%), Space: 260.1 MB (18.68%) - LeetHub

---
+++ 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+        map<int,int>freq;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=target[i]){
+                freq[nums[i]]++;
+            }
+        }
+        return freq.size();
+        
+    }
+};
