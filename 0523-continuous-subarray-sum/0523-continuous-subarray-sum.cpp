3fbe01bc94f25a249c137333ef021d87aca35383
K CHARAN TEJA <charanchowdary644@gmail.com>
2026-08-04 14:58:40 +0530

    Time: 149 ms (6.73%), Space: 148.1 MB (15.2%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        if(nums.size()==1){
+            return 0;
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]%k;
+        }
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==0&&nums[i+1]==0){
+                return 1;
+            }
+        }
+        map<int,int>freq;
+        long long s=0;
+        freq[0]++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                continue;
+            }
+            s+=nums[i];
+            int op=s%k;
+            if(freq.count(op)){
+                return 1;
+            }
+            freq[op]++;
+        }
+        return 0;
+    }
+};
